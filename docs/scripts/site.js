a1a295e52b0299906a85de71fdf150ad65c7562c
ellemenno <ellemenno@users.noreply.github.com>
2017-03-11 14:28:11 -0500

    reduce css and js footprints by requiring individual files

--- docs/scripts/site.js
+++ docs/scripts/site.js
@@ -1,6 +1,16 @@
 ---
+layout: compress
 ---
 
-{% include scripts/jquery-3.1.1.min.js %}
-{% include scripts/semantic-2.2.7.min.js %}
-{% include scripts/semantic-history.min.js %}
+{% include scripts/jquery/jquery-3.1.1.min.js %}
+
+{% include scripts/semantic-ui/accordion.min.js %}
+{% include scripts/semantic-ui/history.min.js %}
+{% include scripts/semantic-ui/search.min.js %}
+{% include scripts/semantic-ui/sidebar.min.js %}
+{% include scripts/semantic-ui/site.min.js %}
+{% include scripts/semantic-ui/state.min.js %}
+{% include scripts/semantic-ui/sticky.min.js %}
+{% include scripts/semantic-ui/tab.min.js %}
+{% include scripts/semantic-ui/transition.min.js %}
+{% include scripts/semantic-ui/visibility.min.js %}
