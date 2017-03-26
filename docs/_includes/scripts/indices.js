16edecf3504a79a6b3b3110d37b41058c7e338fb
ellemenno <ellemenno@users.noreply.github.com>
2017-03-26 17:17:58 -0400

    add shortcut keys to go to collections by first letter

--- docs/_includes/scripts/indices.js
+++ docs/_includes/scripts/indices.js
@@ -3,7 +3,15 @@ collection_tabs_init = function() {
     history: true,
     historyType: 'hash',
   });
-  $('.ui.accordion').accordion();
+
+  // set up shortut key sequences to change tabs
+  {% for collection in site.collections %}
+  {% unless collection.label == 'posts' %}
+  {% capture label %}{{ collection.label | downcase }}{% endcapture %}
+  {% capture key %}{{ label | split: '' | first }}{% endcapture %}
+  Mousetrap.bind('g {{ key }}', function() { window.location.hash = '#/{{ label }}/'; return false; });
+  {% endunless %}
+  {% endfor %}
 };
 
 $(collection_tabs_init);
